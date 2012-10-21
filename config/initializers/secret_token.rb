b149709210a623327af3e0b1b56cd8d51523e501
Joey Kirk <joey.kirk@gmail.com>
2012-10-21 00:21:59 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlocPosting::Application.config.secret_token = '2dab7dffaee0ffd126507c73b74bcfb93d9f4e987c2b7fcf27ab471d90929aab431be9636114a5679e0550667b0048a37e42ba3889b878acd0fb7b3aaaaeed45'
